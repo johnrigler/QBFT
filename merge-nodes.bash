c6f4e2cca88d792b393184746bd3e394fb53b541
John Rigler <john.rigler@protonmail.com>
2022-10-18 02:06:31 +0000

    Tried to make merge-nodes.bash make more sense.

--- merge-nodes.bash
+++ merge-nodes.bash
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 merge-nodes() {
+
+# HOST may need to be 127.0.0.1, localhost, or an IP.
+
+HOST=`hostname`
 cd ./$1
-pwd 
 ls *.json
-# FIRST=$(cat static-nodes.json | grep -v ']')
-# echo $FIRST","
-cat new-static-nodes.json
+cat new-static-nodes.json | grep -v '\[' >> $$.last
+
+(
+cat static-nodes.json | head -n -1 | sed '$ s/$/,/'
+cat $$.last | sed "s/HOST>/$HOST/" | sed 's/<//g'
+) > final-static-nodes.json 
+rm $$.*
 }
 
-merge-nodes $1
+merge-nodes "Node-0/data" 
+
+# This is how you pass in a location
+# merge-nodes $1
